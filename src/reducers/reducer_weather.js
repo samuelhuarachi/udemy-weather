dd357a10782450df6d30aea81ff6716a2ad2a4e4
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2018-04-19 22:39:10 -0300

    renderizando a table

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,8 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
 
-    console.log('Action received', action);
+export default function(state = [], action) {
 
-    
+    switch (action.type) {
+        case FETCH_WEATHER:
+            return [ action.payload.data, ...state ];
+    }
 
 
     return state;
